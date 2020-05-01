549b15316248621d22302de3ebedddd9a30fa305
Lucas Easley <lucaseasley11@gmail.com>
2020-05-01 14:25:01 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,17 +2,17 @@
 # require libraries/modules here
 require "nokogiri"
 require "pry"
- 
+
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   binding.pry
 end
- 
+
 create_project_hash
 
 # projects = {}
-# 
+#
 # kickstarter.css("li.project.grid_4").each do |project|
 #   title = project.css("h2.bbcard_name strong a").text
 #   projects[title] = {
@@ -22,8 +22,8 @@ create_project_hash
 #     :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 #   }
 # end
-# 
+#
 # projects
-# 
-# 
+#
+#
 # end
